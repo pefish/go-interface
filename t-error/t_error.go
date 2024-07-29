cb4e5a644508dc89aa939cbbd4bd5fa80f20da41
pefish <darrondarron@qq.com>
2024-07-29 19:00:00 +0800

    v0.0.10

--- t-error/t_error.go
+++ t-error/t_error.go
@@ -1,7 +1,21 @@
 package t_error
 
+import "fmt"
+
 type ErrorInfo struct {
 	Code uint64      `json:"code"`
 	Data interface{} `json:"data"`
 	Err  error       `json:"err"`
+}
+
+func (ei *ErrorInfo) String() string {
+	return fmt.Sprintf(`ErrorInfo -> msg: %s, code: %d, data: %#v, err: %+v`, ei.Err.Error(), ei.Code, ei.Data, ei.Err)
+}
+
+func (ei *ErrorInfo) Error() string {
+	return ei.String()
+}
+
+func (ei *ErrorInfo) Unwrap() error {
+	return ei.Err
 }
