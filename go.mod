a95f5e45321a327c53fc049e41795c8626c67c1a
pefish <darrondarron@qq.com>
2024-07-29 19:27:56 +0800

    v0.0.11

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pefish/go-interface
 
 go 1.21
+
+require github.com/pkg/errors v0.9.1
--- t-error/t_error.go
+++ t-error/t_error.go
@@ -1,6 +1,10 @@
 package t_error
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
 
 type ErrorInfo struct {
 	Code uint64      `json:"code"`
@@ -18,4 +22,71 @@ func (ei *ErrorInfo) Error() string {
 
 func (ei *ErrorInfo) Unwrap() error {
 	return ei.Err
+}
+
+var (
+	INTERNAL_ERROR_MSG         = "Internal error."
+	INTERNAL_ERROR_CODE uint64 = 1
+	NETWORK_ERROR_MSG          = "Network error."
+	NETWORK_ERROR_CODE  uint64 = 2
+
+	INTERNAL_ERROR = WrapWithCode(fmt.Errorf(INTERNAL_ERROR_MSG), INTERNAL_ERROR_CODE)
+	NETWORK_ERROR  = WrapWithCode(fmt.Errorf(NETWORK_ERROR_MSG), NETWORK_ERROR_CODE)
+)
+
+func Recover(fun func(*ErrorInfo)) {
+	if err := recover(); err != nil {
+		switch e := err.(type) {
+		case *ErrorInfo:
+			fun(e)
+		case error:
+			fun(WrapWithAll(e, INTERNAL_ERROR_CODE, nil))
+		default:
+			fun(WrapWithAll(errors.New(fmt.Sprint(err)), INTERNAL_ERROR_CODE, nil))
+		}
+	}
+}
+
+func WithStack(err error) error {
+	if _, ok := err.(interface {
+		WithStack(err error) error
+	}); !ok { // 如果是不带堆栈的err，则附加上堆栈
+		return errors.WithStack(err)
+	}
+	return err
+}
+
+func ThrowInternal(err error) {
+	var errorInfo_ = ErrorInfo{Code: INTERNAL_ERROR_CODE, Data: nil, Err: WithStack(err)}
+	panic(&errorInfo_)
+}
+
+func ThrowWithCode(err error, code uint64) {
+	var errorInfo_ = ErrorInfo{Code: code, Data: nil, Err: WithStack(err)}
+	panic(&errorInfo_)
+}
+
+func ThrowWithCodeAndData(err error, code uint64, data error) {
+	var errorInfo_ = ErrorInfo{Code: code, Data: data, Err: WithStack(err)}
+	panic(&errorInfo_)
+}
+
+func WrapWithAll(err error, code uint64, data interface{}) *ErrorInfo {
+	return &ErrorInfo{Code: code, Data: data, Err: WithStack(err)}
+}
+
+func WrapWithErr(err error) *ErrorInfo {
+	return &ErrorInfo{Code: INTERNAL_ERROR_CODE, Data: nil, Err: WithStack(err)}
+}
+
+func WrapWithStr(str string) *ErrorInfo {
+	return &ErrorInfo{Code: INTERNAL_ERROR_CODE, Data: nil, Err: WithStack(fmt.Errorf(str))}
+}
+
+func Wrap(err error) *ErrorInfo {
+	return &ErrorInfo{Code: INTERNAL_ERROR_CODE, Data: nil, Err: WithStack(err)}
+}
+
+func WrapWithCode(err error, code uint64) *ErrorInfo {
+	return &ErrorInfo{Code: code, Data: nil, Err: WithStack(err)}
 }
